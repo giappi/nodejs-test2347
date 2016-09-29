1bd0aa6eeecbce3c430ce63d829ac00b27fddcda
giappi <bantraidat@gmail.com>
2016-09-29 08:27:07 +0000

    - Test PHP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = 8080; //process.env.PORT || 3000;
 
 server.listen(port, function()
 {
@@ -11,7 +11,7 @@ server.listen(port, function()
 });
 
 // Routing
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // Chatroom
 
