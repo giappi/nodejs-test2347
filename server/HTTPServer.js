ad34c5d257eca41c4d7334a58ad7655fc13bfefc
giappi <bantraidat@gmail.com>
2016-09-29 10:33:55 +0000

    - big change. restructure code

---
+++ server/HTTPServer.js
@@ -0,0 +1,44 @@
+
+
+module.exports = function()
+{
+    var port = process.env.PORT || 3000;
+    // Setup basic express server
+    var Express = require('express');
+    var appListener = new Express();
+
+    var server = require('http').createServer(appListener);
+    
+    /**
+     * Start to listen
+     */
+    this.start = function()
+    {
+        server.listen(port, function()
+        {
+            console.log('Server has started and listening at port %d', port);
+        });
+        
+        
+    };
+    
+    this.setDocumentRoot = function(path)
+    {
+        // root of website
+        appListener.use(Express.static(path));
+    };
+    
+    this.stop = function()
+    {
+        server.close(function()
+        {
+            console.log('Server closed.');
+        });
+    };
+
+    
+    this.getServer = function()
+    {
+        return server;
+    };
+};
